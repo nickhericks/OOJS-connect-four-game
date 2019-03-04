6b218f9c73fe96fc524b04bd0b66423ab42401d9
Nick-Hericks <nickhericks@gmail.com>
2019-03-04 10:59:25 +0200

    Animate github ribbon

--- js/app.js
+++ js/app.js
@@ -1,25 +1,23 @@
-
 const game = new Game();
 
 /**
- * Listens for click on `#begin-game` and calls startGame() on game object
+ * Listen for click on '#begin-game' and calls startGame() on game object
  */
 document.querySelector('#begin-game').addEventListener('click', function() {
-  // Initializes game
-  game.startGame();
-  // hides start button element
-  this.style.display = 'none';
-  document.getElementById('play-area').style.opacity = '1';
+	// Initializes game
+	game.startGame();
+	// hides start button element
+	this.style.display = 'none';
+	document.getElementById('play-area').style.opacity = '1';
 });
 
 /**
  * Listen for keyboard presses
  */
 document.addEventListener('keydown', function(event) {
-  game.handleKeydown(event);
-  console.log(event.key);
+	game.handleKeydown(event);
+	console.log(event.key);
 });
-
 
 // console.log(game);
 // console.log(game.board);
